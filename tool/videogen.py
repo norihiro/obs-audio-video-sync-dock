3e704ac1d816fbb793581d05ad1f6c45ce60218e
Norihiro Kamae <norihiro@nagater.net>
2023-11-19 22:03:34 +0900

    tool: Add sync pattern index to video and audio
    To find associated video pattern from audio pattern or vice versa, put
    an 8-bit incrementing index number to the pattern.
    - QR code now has an index parameter `i`.
    - Audio has a PSK encoded 8-bit index number + 8-bit preamble pattern.
    
    Also changes audio start timing to be aligned to the video sync timing.
    This change is made with users in mind, who want to check recorded video
    frames and waveform by eyes.

--- tool/videogen.py
+++ tool/videogen.py
@@ -12,6 +12,9 @@ from PIL import Image
 import qrcode
 
 
+TYPE_AUDIO_START_AT_SYNC = 1
+
+
 def _div_ceil(n, m):
     return (n + m - 1) // m
 
@@ -26,6 +29,7 @@ class Context:
     - width -- Video width
     - height -- Video height
     '''
+    # pylint: disable=too-many-instance-attributes
     def __init__(self, workdir, vr, ar):
         '''
         Initializer
@@ -44,6 +48,7 @@ class Context:
         self.height = 720
         self._img_index = 0
         self._sync_image_cache = {}
+        self.type_flags = TYPE_AUDIO_START_AT_SYNC
 
     def next_image(self):
         '''
@@ -81,20 +86,16 @@ class Pattern:
     def __init__(self, ctx, settings):
         self.ctx = ctx
         self._prepare(settings)
-        self.video_cache = None
-        self.audio_cache = None
+        self.i = 0
 
     def _prepare(self, settings):
         q = 2
-        t = 2
         f = 442
         c = 0
         for s in settings.split(','):
             k, v = s.split('=', 1)
             if k == 'q':
                 q = int(v)
-            elif k == 't':
-                t = int(v)
             elif k == 'f':
                 f = int(v)
             elif k == 'c':
@@ -102,25 +103,24 @@ class Pattern:
             else:
                 raise ValueError(f'Invalid keyword {k} in {settings}')
         if c <= 0:
-            c = t * f * self.ctx.vr[1] // self.ctx.vr[0]
-        # Needs t / vr >= c / f
+            c = q * f * self.ctx.vr[1] // (self.ctx.vr[0] * 8)
+        # Needs q / vr >= c / f / 8
         if c <= 0:
-            min_f = _div_ceil(self.ctx.vr[0], t * self.ctx.vr[1])
+            min_f = _div_ceil(self.ctx.vr[0] * 8, q * self.ctx.vr[1])
             raise ValueError(f'{settings}: Audio frequency is too slow, required at least {min_f}')
-        if t * f * self.ctx.vr[1] < c * self.ctx.vr[0]:
-            audio_duration = c / f
-            video_duration = t * self.ctx.vr[1] / self.ctx.vr[0]
+        if q * f * self.ctx.vr[1] < c * 8 * self.ctx.vr[0]:
+            audio_duration = c * 16 / f
+            video_duration = q * 2 * self.ctx.vr[1] / self.ctx.vr[0]
             raise ValueError('Too short video; '
                              + f'video marker duration {video_duration} second, '
                              + f'audio marker duration {audio_duration} second')
-        self.q, self.t, self.f, self.c = q, t, f, c
+        self.q, self.f, self.c = q, f, c
 
-    def _gen_qrcode(self):
+    def _gen_qrcode(self, i):
         ctx = self.ctx
         f, c = self.f, self.c
         q_ms = self.q * 1000 * ctx.vr[1] // ctx.vr[0]
-        t_ms = self.q * 1000 * ctx.vr[1] // ctx.vr[0]
-        qr_img = qrcode.make(f'q={q_ms},t={t_ms},f={f},c={c}',
+        qr_img = qrcode.make(f'q={q_ms},i={i & 0xFF},f={f},c={c},t={ctx.type_flags}',
                              error_correction=qrcode.constants.ERROR_CORRECT_M).get_image()
         size = min(ctx.width, ctx.height)
         qr_img = qr_img.resize((size, size))
@@ -135,38 +135,48 @@ class Pattern:
         '''
         Returns a list of video frame files
         '''
-        if self.video_cache:
-            return self.video_cache
-        qr_filename = self._gen_qrcode()
+        qr_filename = self._gen_qrcode(self.i)
         r_qr = [qr_filename] * self.q
-        r_s0 = [self.ctx.sync_image(0)] * self.t
-        r_s1 = [self.ctx.sync_image(1)] * self.t
-        self.video_cache =  r_qr + r_s0 + r_s1
-        return self.video_cache
+        r_s0 = [self.ctx.sync_image(0)] * self.q
+        r_s1 = [self.ctx.sync_image(1)] * self.q
+        frames = r_qr + r_s0 + r_s1
+        self.i += 1
+        return frames
 
-    def audio_frames(self):
+    def video_frame_length(self):
         '''
-        Returns a audio frame bytes
+        Returns the number of the video frames
         '''
-        if self.audio_cache:
-            return self.audio_cache
-        q, t, f, c = self.q, self.t, self.f, self.c
+        return self.q * 3
+
+    def audio_frames(self, start_offset=0):
+        '''
+        Returns audio frame in bytes
+        '''
+        f, c = self.f, self.c
         ctx = self.ctx
-        n_center = ctx.ar * (q + t) * ctx.vr[1] // ctx.vr[0]
-        n_all = ctx.ar * (q + t * 2) * ctx.vr[1] // ctx.vr[0]
+
+        data = 0xf000 | (self.i & 0xFF)
+
+        n_center = ctx.ar * (self.q * 2) * ctx.vr[1] // ctx.vr[0]
+        n_pattern = 16 * c * ctx.ar // f
+        n_blank_begin = n_center
+        if (ctx.type_flags & TYPE_AUDIO_START_AT_SYNC) == 0:
+            n_blank_begin -= n_pattern // 2
         i = 0
-        buffer = b''
-        while i < n_all:
-            phase = abs(i - n_center)
-            if phase * f < c * ctx.ar:
-                phase = phase % ctx.ar
-                sample = math.sin(phase * 2 * math.pi * f / ctx.ar)
-                sample = round(sample * 32767)
-            else:
-                sample = 0
+        if n_blank_begin < start_offset:
+            raise ValueError(f'Too large start_offset; {start_offset}, expect <= {n_blank_begin}')
+        buffer = b'\x00\x00' * (n_blank_begin - start_offset)
+        while i < n_pattern:
+            phase = i * 2 * math.pi * f / ctx.ar
+            sample = math.sin(phase)
+            i_data = i * f // (ctx.ar * c)
+            if data & (0x8000 >> i_data):
+                sample = -sample
+            sample = round(sample * 32767)
             buffer += (sample & 0xFFFF).to_bytes(2, 'little')
             i += 1
-        self.audio_cache = buffer
+        self.i += 1
         return buffer
 
 
@@ -197,12 +207,13 @@ class VideoGen:
     def _generate_video(self, i0_fmt, n_repeat):
         i = 0
         i_frame = 0
-        while i < n_repeat:
-            for p in self.patterns:
+        for p in self.patterns:
+            p.i = 0
+            while i < n_repeat:
                 for f in p.video_frames():
                     os.link(f, i0_fmt % i_frame)
                     i_frame += 1
-            i += 1
+                i += 1
 
     def _generate_audio(self, i1_name, n_repeat):
         ctx = self.ctx
@@ -210,31 +221,40 @@ class VideoGen:
             i = 0
             i_audio_frame = 0
             i_video_frame = 0
-            while i < n_repeat:
-                for p in self.patterns:
-                    buf = p.audio_frames()
+            for p in self.patterns:
+                p.i = 0
+                while i < n_repeat:
+                    i_audio_exp = i_video_frame * ctx.vr[1] * ctx.ar // ctx.vr[0]
+                    buf = p.audio_frames(start_offset=i_audio_frame - i_audio_exp)
                     i1.write(buf)
                     i_audio_frame += len(buf) // 2
-                    i_video_frame += len(p.video_frames())
+                    i_video_frame += p.video_frame_length()
                     # i_audio_frame / ctx.ar < i_video_frame * ctx.vr[1] / ctx.vr[0]
-                    if i_audio_frame * ctx.vr[0] < i_video_frame * ctx.vr[1] * ctx.ar:
-                        i1.write(int(0).to_bytes(2, 'little'))
+                    while i_audio_frame * ctx.vr[0] < i_video_frame * ctx.vr[1] * ctx.ar:
+                        i1.write(b'\x00\x00')
                         i_audio_frame += 1
-                i += 1
+                    i += 1
 
     def output(self, filename, duration=0, max_duration=0):
         '''
         Outputs to file
         '''
         ctx = self.ctx
-        video_frames = []
+        video_frame_length = 0
         for p in self.patterns:
-            video_frames += p.video_frames()
-        video_unit_sec = (len(video_frames) * ctx.vr[1], ctx.vr[0])
+            video_frame_length += p.video_frame_length()
+        video_unit_sec = (video_frame_length * ctx.vr[1], ctx.vr[0])
         video_unit_sec = _simplify_fraction(video_unit_sec)
 
         if not duration:
             duration = _calculate_duration(video_unit_sec[0], video_unit_sec[1], ctx.ar)
+            if duration > max_duration:
+                duration = video_unit_sec[0] * 256 / video_unit_sec[1]
+            while (duration * video_unit_sec[1] < video_unit_sec[0] * 256) and \
+                    (duration * 2 < max_duration):
+                print(f'Info: multiplying duration={duration}')
+                duration *= 2
+            print(f'Info: duration={duration}')
         n_repeat = _div_ceil(duration * video_unit_sec[1], video_unit_sec[0])
         if n_repeat * video_unit_sec[0] > max_duration * video_unit_sec[1]:
             n_repeat = max_duration * video_unit_sec[1] // video_unit_sec[0]
@@ -253,6 +273,7 @@ class VideoGen:
 
         ffmpeg_cmd = ['ffmpeg']
         ffmpeg_cmd += ['-hide_banner']
+        ffmpeg_cmd += ['-loglevel', 'warning']
         if ctx.vr[1] == 1:
             ffmpeg_cmd += ['-framerate', '%d'%ctx.vr[0]]
         else:
@@ -260,6 +281,7 @@ class VideoGen:
         ffmpeg_cmd += ['-i', i0_fmt]
         ffmpeg_cmd += ['-f', 's16le', '-ac', '1', '-ar', '%d'%ctx.ar, '-i', i1_name]
         ffmpeg_cmd += ['-pix_fmt', 'yuv420p']
+        ffmpeg_cmd += ['-b:a', '192k']
         ffmpeg_cmd += ['-y', filename]
         subprocess.run(ffmpeg_cmd, check=True)
         os.unlink(i1_name)
@@ -270,8 +292,7 @@ _help_patterns = '''
 patterns:
   The pattern specifier is a comma (,) separated list of parameters.
   Each parameter consist of keys listed below and '=' and its value.
-  - q -- Number of video frames for the QR code
-  - t -- Number of video frames for the synchronization marker
+  - q -- Number of video frames for the QR code and marker patterns
   - f -- Frequency of the audio synchronization marker
   - c -- Number of cycles of the audio synchronization marker'''
 
