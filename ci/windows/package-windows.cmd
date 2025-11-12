a5b1746366e4453bbca8fefe4c539013d8672641
Norihiro Kamae <norihiro@nagater.net>
2025-11-12 17:08:32 +0900

    .github: Package deps/quirc/LICENSE

--- ci/windows/package-windows.cmd
+++ ci/windows/package-windows.cmd
@@ -8,6 +8,7 @@ set /p PackageVersion=<package-version.txt
 del package-version.txt
 
 copy ..\LICENSE          ..\release\data\obs-plugins\%PluginName%\LICENCE-%PluginName%.txt
+copy ..\deps\quirc\LICENSE ..\release\data\obs-plugins\%PluginName%\LICENCE-quirc.txt
 
 REM Package ZIP archive
 7z a "%PluginName%-%PackageVersion%-obs%1-Windows.zip" "..\release\*"
